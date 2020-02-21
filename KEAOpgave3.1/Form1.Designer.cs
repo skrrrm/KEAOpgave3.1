35984bcf39a520be5269ac9c91df0a144f5e860c
skrrrm <a@macbook-windows>
2020-02-21 08:45:06 +0100

    Add project files.

---
+++ KEAOpgave3.1/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace KEAOpgave3._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label = new System.Windows.Forms.Label();
+            this.textBoxNumber = new System.Windows.Forms.TextBox();
+            this.buttonRes = new System.Windows.Forms.Button();
+            this.labelRes = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(177, 111);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(164, 25);
+            this.label.TabIndex = 0;
+            this.label.Text = "Write a number:";
+            // 
+            // textBoxNumber
+            // 
+            this.textBoxNumber.Location = new System.Drawing.Point(359, 111);
+            this.textBoxNumber.Name = "textBoxNumber";
+            this.textBoxNumber.Size = new System.Drawing.Size(100, 31);
+            this.textBoxNumber.TabIndex = 1;
+            // 
+            // buttonRes
+            // 
+            this.buttonRes.Location = new System.Drawing.Point(308, 172);
+            this.buttonRes.Name = "buttonRes";
+            this.buttonRes.Size = new System.Drawing.Size(151, 42);
+            this.buttonRes.TabIndex = 2;
+            this.buttonRes.Text = "button1";
+            this.buttonRes.UseVisualStyleBackColor = true;
+            this.buttonRes.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelRes
+            // 
+            this.labelRes.AutoSize = true;
+            this.labelRes.Location = new System.Drawing.Point(177, 247);
+            this.labelRes.Name = "labelRes";
+            this.labelRes.Size = new System.Drawing.Size(0, 25);
+            this.labelRes.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelRes);
+            this.Controls.Add(this.buttonRes);
+            this.Controls.Add(this.textBoxNumber);
+            this.Controls.Add(this.label);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.TextBox textBoxNumber;
+        private System.Windows.Forms.Button buttonRes;
+        private System.Windows.Forms.Label labelRes;
+    }
+}
+
---
+++ KEAOpgave3.1/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KEAOpgave3._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Variabler
+        double n;
+        string res;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
